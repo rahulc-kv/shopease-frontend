ce40ae626a801fdd099f2c8bb9c3708974f63d55
rahulc97 <rahul.c@keyvalue.systems>
2023-09-15 09:51:29 +0530

    removed unwanted code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 
-import { Cards, Container, Footer, Header, Main } from "components";
-
 const Home: React.FC = () => {
   return (
-    <Container>
-      <Header />
-      <Main />
-      <Cards />
-      <Footer />
-    </Container>
+    <div>
+      main page
+    </div>
   );
 };
 
